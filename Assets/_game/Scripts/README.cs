1dac47a8bf85c83c2fa6ce8f7867c317649750c1
Owen Mundy <omundy@gmail.com>
2020-03-21 09:11:38 -0400

    Adds base Unity project

---
+++ Assets/_game/Scripts/README.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *  README for DIG 250 Base Unity Project
+ *  - This base project probably contains more folders than you need; delete as required
+ *  - Project folders are organized in a _game folder to prevent overlap/confusion with
+ *    imported assets from other developers
+ *  - The .gitignore file copied from https://www.gitignore.io/api/unity,macos,windows
+ */
+
+public class README : MonoBehaviour
+{
+    void Start()
+    {
+        print ("hello world!");
+    }
+
+}
