d5f4e91de55eaa5437ebd47d7d0caa340a9cddd0
AndrewHoyle1 <51134718+AndrewHoyle1@users.noreply.github.com>
2020-03-23 16:52:53 -0400

    Made an input manager

---
+++ Assets/_game/Scripts/InputManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ButtonAssignments
+{
+    Left,
+    Right
+}
+
+public enum Condition
+{
+    GreaterThan,
+    LessThan
+}
+
+[System.Serializable]
+public class InputAxisState
+{
+    public string axisName;
+    public float offValue;
+    public ButtonAssignments button;
+    public Condition condition;
+    public bool value
+    {
+        get
+        {
+            var val = Input.GetAxis(axisName);
+            switch(condition)
+            {
+                case Condition.GreaterThan:
+                    return val > offValue;
+                case Condition.LessThan:
+                    return val < offValue;
+            }
+            return false;
+        }
+    }
+}
+
+public class InputManager : MonoBehaviour
+{
+    public InputAxisState[] inputs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var input in inputs)
+        {
+            if (input.value)
+                Debug.Log("Input Detected" + input.button);
+        }
+    }
+}
