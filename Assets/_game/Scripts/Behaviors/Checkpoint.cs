f68410800646874ed4553422c930a582b2987968
BigFelipe <Dagove@davidson.edu>
2020-04-24 12:21:18 -0400

    New Animation Stuff

--- Assets/_game/Scripts/Behaviors/Checkpoint.cs
+++ Assets/_game/Scripts/Behaviors/Checkpoint.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Checkpoint : MonoBehaviour
+public class Checkpoint : MonoBehaviour //I actually think this stuff should all go into CollisionState
 {
     public bool triggered;
     // Start is called before the first frame update
@@ -14,7 +14,7 @@ public class Checkpoint : MonoBehaviour
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.gameObject.tag == "Checkpoint")
+        if (collider.gameObject.tag == "Checkpoint" && triggered == false)
         {
             triggered = true;
         }
---
+++ Assets/_game/Scripts/LevelMilestones/ActivateCheckpoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateCheckpoint : MonoBehaviour
+{
+    public bool hasCollided;
+
+    public Animator animator;
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player" && hasCollided == false)
+        {
+            hasCollided = true;            
+            StartCoroutine(animationDelayer());
+        }
+    }
+
+    protected IEnumerator animationDelayer()  //Makes the player able to pass through certain walls for the duration of the dash
+    {
+        print("pre");
+        animator.SetInteger("FlagState", 1);
+        yield return new WaitForSeconds(1);
+        animator.SetInteger("FlagState", 2);
+        print("post");
+    }
+}
