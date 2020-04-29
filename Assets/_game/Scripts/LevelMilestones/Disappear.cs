4fbddd7bd822975bac5bf33e4b5966af713e32ac
BigFelipe <Dagove@davidson.edu>
2020-04-29 11:32:25 -0400

    Disappearing Plat

---
+++ Assets/_game/Scripts/LevelMilestones/Disappear.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Disappear : MonoBehaviour
+{
+    public TilemapRenderer tilemapRenderer;
+    public TilemapCollider2D tilemapCollider;
+    public bool present; //Tells if the plat is present.
+
+    public float presentTime; //How long the plat is active
+    public float goneTime; //How long it's gone
+    private float currentWaitTime; //How long until the next change
+
+    void Start()
+    {
+        currentWaitTime = presentTime;
+        present = true;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        currentWaitTime -= Time.deltaTime;
+        if (currentWaitTime < 0)
+        {
+            if(present == true)
+            {
+                tilemapCollider.enabled = false;
+                tilemapRenderer.enabled = false;
+                present = false;
+                currentWaitTime = goneTime;
+            }
+            else
+            {
+                tilemapCollider.enabled = true;
+                tilemapRenderer.enabled = true;
+                present = true;
+                currentWaitTime = presentTime;
+            }
+        }
+    }
+
+
+}
