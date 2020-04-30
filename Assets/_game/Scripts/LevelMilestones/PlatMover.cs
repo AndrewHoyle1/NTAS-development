08265d753de7b077702535ebc906bb540fac9db7
BigFelipe <Dagove@davidson.edu>
2020-04-29 20:40:51 -0400

    Moving PLat

---
+++ Assets/_game/Scripts/LevelMilestones/PlatMover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatMover : MonoBehaviour
+{
+    public float speed = 10f;
+    public float turnTime = 5f; //How long in between turns
+
+    private float direction;
+    private float timeLeft; //Current time until it turns around
+
+    public Rigidbody2D body2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = turnTime;
+        direction = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft > 0)
+        {
+            body2d.velocity = new Vector2(speed * direction, 0);
+        }
+        else
+        {
+            direction = direction * (-1);
+            timeLeft = turnTime;
+        }
+    }
+}
