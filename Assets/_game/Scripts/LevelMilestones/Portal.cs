387018209dbe4fb407150fef26c1f870ab243d40
BigFelipe <Dagove@davidson.edu>
2020-05-05 23:21:53 -0400

    Improved Menus

---
+++ Assets/_game/Scripts/LevelMilestones/Portal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            print("Scene Transition");
+            SceneManager.LoadScene("Start Menu");
+        }
+    }
+}
